66c31b3e1072cd725b6b6b55cbdc9bd819bfd68b
jerome-shiftleft <jerome@theshiftleft.com>
2024-01-17 03:01:00 +0800

    progress

--- routes/admin.js
+++ routes/admin.js
@@ -1,23 +1,28 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
 const products = [];
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
+router.get("/add-product", (req, res, next) => {
   //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render("add-product", { pageTitle: "Add Product", path: '/admin/add-product' });
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    activeProduct: true,
+    formsCSS: true
+  });
 });
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({title: req.body.title});
-  res.redirect('/');
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
 });
 
 // module.exports = {
